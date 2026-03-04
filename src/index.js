b450d28dfe4034975ea2415d19c8ef961138e633
Piyush Kamlakar <you@example.com>
2026-03-04 22:19:16 +0530

    basic api and winston logging added to the project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-const {PORT} = require("./config");
+const {ServerConfig} = require("./config");
+const apiRoutes = require("./routes");
 
-app.listen(PORT,()=>{
-    console.log(`App is Active and listening on Port No: ${PORT}`);
+
+app.use('/api',apiRoutes);
+
+app.listen(ServerConfig.PORT,()=>{
+    console.log(`App is Active and listening on Port No: ${ServerConfig.PORT}`);
 })
