6867d0f4f61aaca83f9416e5bf01905e66698996
Piyush Kamlakar <you@example.com>
2026-03-08 12:48:08 +0530

    pushed in the Seeder file

---
+++ src/seeders/20260306160034-add-airplanes.js
@@ -0,0 +1,40 @@
+'use strict';
+const {Op} = require("sequelize");
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert("Airplanes",[
+      {
+      modelNumber:"Airbus380",
+      capacity:200,
+      createdAt:new Date(),
+      updatedAt:new Date(),
+      },
+      {
+        modelNumber:"Boeing900",
+        capacity:150,
+        createdAt:new Date(),
+        updatedAt:new Date(),
+      }
+  ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Airplanes',{[Op.or]:[{modelNumber:"Airbus380"},{modelNumber:"Boeing900"}]});
+  }
+};
