4551879d76e4a7fcd489aefe4784c8c2ef9f267b
Piyush Kamlakar <you@example.com>
2026-03-10 20:13:48 +0530

    added new migration that can add constraint in a  column of airport table

---
+++ src/migrations/20260309152037-update-city-airport-association.js
@@ -0,0 +1,47 @@
+// 'use strict';
+
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     await queryInterface.addConstraint('Airports',{
+//       type:'FOREIGN KEY',
+//       name:'city_fkey_constraint',
+//       // options.feilds:['cityId'],
+//       options.fields:['cityid'],
+//       refenrences:{
+//         table:'Cities',
+//         feild:'id'
+//       },
+//       onUpdate:'CASCADE',
+//       onDelete:'CASCADE'
+//     })
+//   },
+
+//   async down (queryInterface, Sequelize) {
+//     await queryInterface.removeConstraint('Airports','city_fkey_constraint')
+//   }
+// };
+
+
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addConstraint('Airports', {
+      type: 'foreign key',
+      name: 'city_fkey_constraint',
+      fields: ['cityId'], 
+      references: {
+        table: 'Cities',
+        field: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint('Airports', 'city_fkey_constraint');
+  }
+};
